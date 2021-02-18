4f1639c3f29e55aae44bc5037ea023d1a2761184
James Post <avilapost93@gmail.com>
2021-02-18 06:52:13 -0800

    previous days work; wroking on object exception with gql pull on queries

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,7 +9,7 @@ import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
   const { user } = useContext(AuthContext);
-  const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY)
+  const { loading, data: { getPosts: posts }} = useQuery(FETCH_POSTS_QUERY)
 
   if(posts){
       console.log(posts)
--- client/src/util/graphql.js
+++ client/src/util/graphql.js
@@ -1,7 +1,7 @@
-import gql from 'graphql-tag';
+import  { gql } from '@apollo/client';
 
 export const FETCH_POSTS_QUERY = gql`
-  {
+  query{
     getPosts {
       id
       body
