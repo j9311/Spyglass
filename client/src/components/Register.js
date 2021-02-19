dca0e0125a230ce5b7520b2516b6b2a36b63b0ca
James Post <avilapost93@gmail.com>
2021-02-19 09:14:51 -0800

    deploy fixes!

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,97 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {Form, Button} from "semantic-ui-react"
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks'
 
 function Register() {
-    return (
-        <div className="cont">
-        <h1>REGISTER</h1>
 
-        </div>
-    )
+    // const[values, setValues] = useState({
+    //     username: '',
+    //     email: '',
+    //     password: '',
+    //     confirmPassword: '',
+
+    // })
+
+    // const onChange = (e) => {
+    //     setValues({...values, [e.target.name]: e.target.value})
+    // }
+
+   
+
+    // const [addUser, {loading}] = useMutation(REGISTER_USER, {
+    //     update(proxy, result){
+    //         console.log(result)
+    //     },
+    //     variables: values
+    //     }
+    // )
+
+    // const onSubmit = (e) => {
+    //     e.preventDefault();
+    //         addUser()
+    // }
+    // return (
+    //     <div className="cont">
+        
+    //         <Form onSubmit={onSubmit} noValidate>
+    //         <h1>REGISTER</h1>
+    //         <Form.Input
+    //         label="Username"
+    //         placeholder="Username..."
+    //         name="username"
+    //         value={values.username}
+    //             onChange={onChange}
+    //         />
+    //         <Form.Input
+    //         label="Email"
+    //         placeholder="Email..."
+    //         name="email"
+    //         value={values.email}
+    //         onChange={onChange}
+    //         />
+    //         <Form.Input
+    //         label="Password"
+    //         placeholder="Password..."
+    //         name="password"
+    //         value={values.password}
+    //         onChange={onChange}
+    //         />
+    //         <Form.Input
+    //         label="Confirm Password"
+    //         placeholder="confirmPassword..."
+    //         name="confirmPassword"
+    //         value={values.confirmPassword}
+    //         onChange={onChange}
+    //         />
+    //         <Button type='submit' primary>
+    //             Register
+    //         </Button>
+    //         </Form>
+    //     </div>
+    // )
 }
+
+
+
+// const REGISTER_USER = gql`
+//     mutation register(
+//         $username: String!
+//         $email: String!
+//         $password: String!
+//         $confirmPassword: String!
+//     ) {
+//         register(registerInput){
+//             username: $username
+//             email: $email
+//             password: $password
+//             confirmPassword: $confirmPassword
+//             }
+//         ){
+//             id email username token
+//         }
+//     }
+
+// `
 
 export default Register;
--- client/src/subcomponents/PostCard.js
+++ client/src/subcomponents/PostCard.js
@@ -6,12 +6,15 @@ import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
 import DeleteButton from './DeleteButton';
-import MyPopup from '../util/MyPopup';
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes }
 }) {
   const { user } = useContext(AuthContext);
+
+  function commentOnPost() {
+    console.log('commented')
+  }
 
   return (
     <Card fluid>
@@ -29,16 +32,14 @@ function PostCard({
       </Card.Content>
       <Card.Content extra>
         <LikeButton user={user} post={{ id, likes, likeCount }} />
-        <MyPopup content="Comment on post">
-          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
-            <Button color="blue" basic>
+          <Button labelPosition="right" as={Link} to={`/posts/${id}`} onClick={commentOnPost}>
+            <Button color="blue">
               <Icon name="comments" />
             </Button>
             <Label basic color="blue" pointing="left">
               {commentCount}
             </Label>
           </Button>
-          </MyPopup>
         {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
