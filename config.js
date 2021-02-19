bd49a0a138862f9629699cac2a25bdeeb46eaab9
James Post <avilapost93@gmail.com>
2021-02-19 09:22:55 -0800

    env workaround

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+      'mongodb+srv://james:onkomcpx69@cluster0.ed5rh.mongodb.net/firebase?retryWrites=true&w=majority',
+    HIDDENKEY: 'top secret, bugger off'
+  };
