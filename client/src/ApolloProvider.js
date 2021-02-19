cfdd6cf02052b9926f15eca0367880f2c6d534aa
James Post <avilapost93@gmail.com>
2021-02-19 11:58:48 -0800

    /client

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -3,11 +3,11 @@ import App from './App';
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 console.log(process.env)
 const httpLink = createHttpLink({
-  uri: 'https://spy-glass.herokuapp.com'
+  uri: process.env.NODE_ENV === "development" ? "http://localhost:5000" : 'https://spy-glass.herokuapp.com'
 });
 
 const authLink = setContext(() => {
